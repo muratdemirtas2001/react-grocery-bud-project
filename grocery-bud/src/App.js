1d3e2bf14bc3a0fbb333d189e320b0b4365cb243
Murat Demirtas <73036794+muratdemirtas2001@users.noreply.github.com>
2020-12-29 20:23:39 +0000

    initializing files

---
+++ grocery-bud/src/App.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+import List from './list'
+import Alert from './alert'
+
+function App() {
+  return <h2>grocery bud setup</h2>
+}
+
+export default App
