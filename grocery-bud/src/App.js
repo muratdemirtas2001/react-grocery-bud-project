a3ff89644f181bf05dfbc3fb42016128efa31445
Murat Demirtas <73036794+muratdemirtas2001@users.noreply.github.com>
2020-12-29 20:58:31 +0000

    working on app.js

--- grocery-bud/src/App.js
+++ grocery-bud/src/App.js
@@ -3,7 +3,35 @@ import List from './list'
 import Alert from './alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name,setName]=useState("");
+  const [list,setList]=useState("[]");
+  const [isEditing,setIsEditing]=useState(false);
+  const [editID,setEditID]=useState(null);
+  const [alert,setAlert]=useState({show:true, msg:"", type:""})
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log("hello")
+  }
+
+  return <section className="section-center">
+    <div className="grocery-container">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert/> }
+        <h3>grocery-bud </h3>
+        <div className="form-control">
+          <input type="text"/>
+          <button type="submit" className="submit-btn">
+            {isEditing? "edit" :"submit"}
+          </button>
+        </div>
+      </form>
+      <List />
+      <button className="clear-btn"> clear items</button>
+
+    </div>
+
+  </section>
 }
 
 export default App
